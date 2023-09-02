654252e89cbc43d86825030502e73d4afc85118e
Jyotirmoy Baidya <jyotirmoybaidya408@gmail.com>
2023-09-02 23:42:36 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 
 
@@ -8,7 +9,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
 
